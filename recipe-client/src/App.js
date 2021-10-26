4e2022b704a5a37df8129d806250cdf672a98746
Kalkidan Alemu <kali.taye@gmail.com>
2021-10-26 19:22:12 -0400

    add sidebar component

--- recipe-client/src/App.js
+++ recipe-client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
 import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
-  return <Sidebar />;
+  return (
+    <Container className="app-container">
+      <Sidebar />
+      <Row></Row>
+    </Container>
+  );
 }
 
 export default App;
