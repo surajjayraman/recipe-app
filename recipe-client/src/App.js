7b506f7e66b100da7d0845e9857f572651496d50
Kalkidan Alemu <kali.taye@gmail.com>
2021-10-26 17:26:21 -0400

    unistall mui

--- recipe-client/src/App.js
+++ recipe-client/src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Box from "@mui/material/Box";
+
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
-  return (
-    <Box sx={{ display: "flex" }}>
-      <Sidebar />
-    </Box>
-  );
+  return <Sidebar />;
 }
 
 export default App;
