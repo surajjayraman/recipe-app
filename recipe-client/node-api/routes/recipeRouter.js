1ffb814b5f63e040bc9601118bdc34c93d2817b4
Kalkidan Alemu <kali.taye@gmail.com>
2021-10-28 20:27:58 -0400

    Add Recipe, ingridients and user routes

---
+++ recipe-client/node-api/routes/recipeRouter.js
@@ -0,0 +1,86 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "vagrant",
+  host: "localhost",
+  database: "dietdash_db",
+  password: "123",
+  port: 5432,
+});
+
+pool.connect(console.log("connected to dietdash db"));
+
+const getRecipes = () => {
+  return new Promise(function (resolve, reject) {
+    pool.query(
+      "SELECT * FROM recipe ORDER BY recipe_id ASC",
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(results.rows);
+      }
+    );
+  });
+};
+const addRecipes = (body) => {
+  return new Promise(function (resolve, reject) {
+    const {
+      user_id,
+      recipe_name,
+      image_url,
+      prep_time,
+      serving_size,
+      cuisine_type,
+      source,
+      preparation,
+      ingredients,
+      meal_type,
+      calories,
+      fat,
+      carbs,
+      protein,
+    } = body;
+    pool.query(
+      "INSERT INTO recipe (user_id, recipe_name, image_url, prep_time, serving_size, cuisine_type, source, preparation, ingredients, meal_type, calories, fat, carbs, protein) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *",
+      [
+        user_id,
+        recipe_name,
+        image_url,
+        prep_time,
+        serving_size,
+        cuisine_type,
+        source,
+        preparation,
+        ingredients,
+        meal_type,
+        calories,
+        fat,
+        carbs,
+        protein,
+      ],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(`A new recipe has been added added: ${results.rows[0]}`);
+      }
+    );
+  });
+};
+const deleteRecipe = () => {
+  return new Promise(function (resolve, reject) {
+    const id = parseInt(request.params.id);
+    pool.query("DELETE FROM recipe WHERE id = $1", [id], (error, results) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(`Merchant deleted with ID: ${id}`);
+    });
+  });
+};
+
+module.exports = {
+  getRecipes,
+  addRecipes,
+  deleteRecipe,
+};
--- recipe-client/node-api/server.js
+++ recipe-client/node-api/server.js
@@ -3,7 +3,53 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+  res.status(200).send("Connected to dietDash database!");
+});
+
+const recipeRouter = require("./routes/recipeRouter.js");
+
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
+  next();
+});
+
+app.get("/recipe", (req, res) => {
+  recipeRouter
+    .getRecipes()
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.post("/recipe", (req, res) => {
+  recipeRouter
+    .addRecipes(req.body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.delete("/recipe/:id", (req, res) => {
+  recipeRouter
+    .deleteRecipe(req.params.id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 });
 
 app.listen(port, () => {
